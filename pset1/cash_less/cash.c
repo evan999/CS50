c2ba96fdcd8d5d1cf94df8f25b2408911d516278
Evan Meshberg <emeshberge@gmail.com>
2018-11-25 21:00:02 +0000

    Implement solution - complete

--- pset1/cash_less/cash.c
+++ pset1/cash_less/cash.c
@@ -1,12 +1,80 @@
-#include <stdio.>
+#include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int main(void)
 {
+    float dollars;
+    int cents;
+    int quarters = 0;
+    int dimes = 0;
+    int nickels = 0;
+    int pennies = 0;
     int change;
+    int coins;
+
     do
     {
-        change = get_float("Change: ");
+        dollars = get_float("Change owed: ");
     }
-    while ();
+    while (dollars < 0);
+
+   // cents = dollars * 100;
+    change = round(dollars * 100);
+
+   // change = round(cents);
+
+    //printf("Dollars: %.2f\n", dollars);
+    printf("Change: %i\n", change);
+   // printf("Cents: %i\n", cents);
+
+    while(change >= 25)
+    {
+        quarters = quarters + 1;
+        change = change - 25;
+    }
+    while(change >= 10 && change < 25)
+    {
+        dimes = dimes + 1;
+        change = change - 10;
+    }
+    while(change >= 5 && change < 10)
+    {
+        nickels = nickels + 1;
+        change = change - 5;
+    }
+    while(change >= 1 && change < 5)
+    {
+        pennies = pennies + 1;
+        change = change - 1;
+    }
+
+    coins = quarters + dimes + nickels + pennies;
+
+
+
+    printf("Quarters used: %i\n", quarters);
+    printf("Dimes used: %i\n", dimes);
+    printf("Nickels used: %i\n", nickels);
+    printf("Pennies used: %i\n", pennies);
+    printf("%i\n", coins);
+
 }
+
+/*
+    while(change >= 0.25)
+    {
+        quarters = quarters + 1;
+        change = change - 0.25;
+    }
+    while(dimes >= 0.25)
+    {
+
+    }
+*/
+   // int round(int cents);
+
+   // printf("%.2d\n", change);
+
+
+
