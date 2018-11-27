79c311d64b8c2d30a27c07df6fd304d528ef64a3
Evan Meshberg <emeshberge@gmail.com>
2018-11-27 02:31:04 +0000

    Started initials problem

---
+++ pset2/initials_less/initials.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    string name = get_string("Enter your name: ");
+
+    for(int i = 0; i < strlen(name); i++)
+    {
+        int first_initial = name[0];
+
+        if(islower(name[0]))
+        {
+            firstI = toupper(name[0]);
+        }
+        else
+        {
+
+        }
+
+        // int last_initial =
+
+    }
+}
---
+++ pset2/initials_less/readme.txt
@@ -0,0 +1,12 @@
+TODO:
+
+Prompt for name:
+    -get_string
+    -assume only letters and spaces
+    -assume single space between letters
+Print initials
+
+Initials:
+-First letter
+-Letters following spaces
+-Capitalize initial
