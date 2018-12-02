8d7d41baa0d5d63ec62cba0e751389d367b20728
Evan Meshberg <emeshberge@gmail.com>
2018-12-02 23:28:20 +0000

    Finished coding solution

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -14,11 +14,12 @@ int main(int argc, string argv[])
     int key = atoi(argv[1]);
     // printf("%i\n", key);
 
-    string plaintext = get_string();
-    // printf("%s\n", plaintext);
+    string plaintext = get_string("plaintext: ");
+    //printf("%s\n", plaintext);
 
     // int ciphertext[strlen(plaintext)];
 
+    printf("ciphertext: ");
 
     for (int i = 0, n = strlen(plaintext); i < n; i++)
     {
@@ -28,17 +29,39 @@ int main(int argc, string argv[])
         {
             if(isupper(plaintext[i]))
             {
+
                 int number = (int) letter;
-                int cipher = number + key % 26;
-                printf("%i\n", cipher);
+                //printf("%i\n", number);
+                int cipher = (number + key % 26) - 65;
+                while(cipher > 25)
+                {
+                    cipher = cipher - 26;
+                }
+                //printf("%i\n", cipher);
+                int ascii_cipher = cipher + 65;
+                char ascii_char = (char) ascii_cipher;
+                printf("%c", ascii_char);
             }
             else if(islower(plaintext[i]))
             {
+                //printf("%c", plaintext[i]);
                 int number = (int) letter;
-                printf("%i\n",  cipher);
+                //printf("%i\n", number);
+                int cipher = (number + key % 26) - 97;
+                while(cipher > 25)
+                {
+                    cipher = cipher - 26;
+                }
+                //printf("%i\n", cipher);
+                int ascii_cipher = cipher + 97;
+                char ascii_char = (char) ascii_cipher;
+                printf("%c", ascii_char);
             }
-            // printf("%c", plaintext[i]);
         }
-
+        else
+        {
+           printf("%c", plaintext[i]);
+        }
     }
+    printf("\n");
 }
