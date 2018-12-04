152c6db643e27599a918291ae19e49f4e8a3485b
Evan Meshberg <emeshberge@gmail.com>
2018-12-04 06:46:15 +0000

    Update Vigenere

---
+++ pset2/vigenere/readme.txt
@@ -0,0 +1,14 @@
+
+Formula:
+
+ci = (pi + kj) % 26
+
+For each character in plaintext:
+
+A = 0, B = 1, C = 2, ... Z = 25
+a = 0, b = 1, c = 2, ... z = 25
+
+a + a = 0 + 0 = 0
+
+Z + Z = 25 + 25 = 50 - 25 = 25;
+
--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -8,71 +8,97 @@ int main(int argc, string argv[])
 {
 
     string key = argv[1];
+    if (argc != 2)
+    {
+        printf("Usage: ./vigenere k\n");
+        return 1;
+    }
+
     for (int k = 0; k < strlen(key); k++)
     {
-        if (argc != 2 || !isalpha(key[k]))
+        if (!isalpha(key[k]))
         {
             printf("Usage: ./vigenere k\n");
-            return 1;
         }
     }
 
     string plaintext = get_string("plaintext: ");
+    int key_len = strlen(key);
+    // plaintext = p
 
     printf("ciphertext: ");
-
-    for (int i = 0, n = strlen(plaintext); i < n; i++)
+    //int CASE;
+    for (int plaintext_index = 0, key_index = 0, n = strlen(plaintext); plaintext_index < n; plaintext_index++, key_index++)
     {
-        char letter = plaintext[i];
-
-        if (isalpha(plaintext[i]))
+        int letterKey = tolower(key[key_index % key_len]) - 'a';
+        if (key_index >= key_len)
         {
-            if (isupper(plaintext[i]))
-            {
-                for(int j = 0; j < strlen(key); j++)
-                {
-                    int key_num = (int) key[i];
-                    int number = (int) letter;
-                    printf("%i\n", key_num);
-                    //printf("%i\n", number);
-                    int cipher = (number + key_num % 26) - 65;
-                    while (cipher > 25)
-                    {
-                        cipher = cipher - 26;
-                    }
-                    //printf("%i\n", cipher);
-                    int ascii_cipher = cipher + 65;
-                    char ascii_char = (char) ascii_cipher;
-                    // printf("%c", ascii_char);
-                }
-
-            }
-            else if (islower(plaintext[i]))
-            {
-                for(int j = 0; j < strlen(key); j++)
-                {
-                    int key_num = (int) key[i];
-                    int number = (int) letter;
-                    //printf("%i\n", key_num);
-                    //printf("%i\n", number);
-                    int cipher = (number + key_num % 26) - 65;
-                    while (cipher > 25)
-                    {
-                        cipher = cipher - 26;
-                    }
-                    //printf("%i\n", cipher);
-                    int ascii_cipher = cipher + 65;
-                    char ascii_char = (char) ascii_cipher;
-                    printf("%c", ascii_char);
-                }
-            }
+            key_index = 0;
         }
+        if (isupper(plaintext[plaintext_index]))
+        {
+            //printf("%c", 'A' + (p[i] - 'A' + letterKey) % 26);
+            printf("%c", 'A' + (plaintext[plaintext_index] - 'A' + letterKey) % 26);
+            key_index++;
+           // CASE = 'a';
+        }
+        else if (islower(plaintext[plaintext_index]))
+        {
+           printf("%c", 'a' + (plaintext[plaintext_index] - 'a' + letterKey) % 26);
+           key_index++;
+            // CASE = 'A';
+        }
+
         else
         {
-            printf("%c", plaintext[i]);
+            printf("%c", plaintext[plaintext_index]);
         }
+
     }
+        // else
+        // {
+        //     printf("%c", p[i]);
+        // }
 
     printf("\n");
     //return 0;
 }
+
+
+            // if (isupper(plaintext[plaintext_index]))
+            // {
+            //     int key_num = (int) key[key_index];
+            //     // printf(" Key num: %i\n", key_num);
+            //     int pnum = (int) plaintext[plaintext_index];
+            //     // printf("Letter: %i\n", pt_num);
+            //     //printf("%i\n", number);
+
+            //     int cipher = ((pnum - 'A') + (key_num - 'A')) % 26;
+
+            //      // c[i] = (p[i] + k[j]) % 26
+            //     if(key_index == strlen(key))
+            //     {
+            //       key_index = 0;
+            //     }
+
+            //     printf("%i", cipher);
+            //     int ascii_cipher = cipher + 65;
+            //     char ascii_char = (char) ascii_cipher;
+            //     //printf("%c", ascii_char);
+            // }
+
+
+            // int key_num = (int) ;
+            // int pnum = (int) ;
+            //printf("key: %i\n", key_num);
+            //printf("p: %i\n", pnum);
+            // int cipher = ;
+            // if(key_index >= strlen(key))
+            // {
+            //   key_index = 0;
+            // }
+
+            // int ascii_cipher = cipher + 97;
+            // char ascii_char = (char) ascii_cipher;
+            //printf("%c", ascii_char);
+
