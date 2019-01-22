c74e1ba1d7f41aa01a22051c0cd477cfdd94594e
Evan Meshberg <emeshberge@gmail.com>
2019-01-22 05:55:02 +0000

    More progress made on algorithm. Still some work to be done

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // TODO
 // Open memory card file
@@ -11,49 +12,88 @@
 
 int main(int argc, char *argv[])
 {
-    // Open memory card file
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: ./recover image\n");
+        return 1;
+    }
+
+    // Unable to open file
+    if (raw_file == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", argv[1]);
+        return 2;
+    }
+
+
+    // Open memory card raw data file with read permission
     FILE *raw_file = fopen(argv[1], "r");
 
-    char* buffer = malloc(sizeof(char) * 512);
-    // Read into memory card
-    while(!feof(raw_file))
+    unsigned char buffer[512]; //save 512 bytes at a time per block into a buffer array
+
+    char filename[8]; //array of chars in filename
+
+    // Output file for writing JPEGs to
+    FILE *img = NULL;
+
+    int jpeg_counter = 0; // counter variable for JPEGs found in file.
+
+    // If JPEG found, set to true
+    bool jpeg_found = false;
+
+    // Read into memory card file
+    while(fread(buffer, sizeof buffer, 1, raw_file) == 1)
     {
         // fread(buffer, 1, 512, raw_file);
 
-        // Read first 4 blocks of header
-
-        if (buffer[0] == 0xff &&
-            buffer[1] == 0xd8 &&
-            buffer[2] == 0xff &&
-            (buffer[3] & 0xf0) == 0xe0)
+        // Read first 4 bytes of header for each block
+        // Determine if block contains a JPEG.
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 &&
+            buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            // header for JPEG file
-            // Read 512
-            fread(buffer, 1, 512, raw_file);
+            //If not first JPEG
+            if (jpeg_found == true)
+            {
+                // found start of new JPEG, close current JPEG
+                fclose(img);
+                // create new JPEG
+                sprintf(filename, "%03i.jpg", jpeg_counter);
+                img = fopen(filename, "w");
+                jpeg_counter++;
+                // close JPEG and open new one once new JPEG is found
 
-            // close JPEG and open new one once new JPEG is found
-            fclose(raw_file);
 
-            // open new file
-            sprintf(filename, "%00i.jpg", i);
-            // named in order the files are found
-            // keep track of files! (how?)
-            // filename: char array to store the resultant string
-            // #filenames: ###.jpg
-            FILE *img = fopen(filename, "w");
+                // open new file
 
-            // write new file 512 bytes at a time until new JPEG
-            // is found
-            fwrite(data, size, number, outptr);
-            // read 512 blocks
+
+                // named in order the files are found
+                // keep track of files! (how?)
+                // filename: char array to store the resultant string
+                // #filenames: ###.jpg
+
+
+                // write new file 512 bytes at a time until new JPEG
+                // is found
+                //fwrite(data, size, number, outptr);
+                // read 512 blocks
+            }
+            else
+            {
+                // first JPEG found
+                jpeg_found = true;
+            }
+
         }
-        else
+        if (jpeg_found == true)
         {
             // not a JPEG
             // Skip ahead 508 bytes to next block
-            fread(buffer, 512, 1, raw_file);
+            // fread(buffer, 512, 1, raw_file);
+            fwrite(&buffer, sizeof buffer, 1, img);
 
         }
+    }
 
 }
 
