02b577f66cbfd446f7789d8270d63cedf5b28fa0
Evan Meshberg <emeshberge@gmail.com>
2019-04-27 17:39:28 +0000

    Implement method to retrieve user input. Needs input validation.

--- pset6/mario/mario.py
+++ pset6/mario/mario.py
@@ -1,1 +1,17 @@
 import cs50
+
+
+pyramid_height = input("Enter the height of the pyramid: ")
+
+def get_pyramid_height():
+
+    while True:
+        try:
+         pyramid_height = input("Enter the height of the pyramid: ")
+        except ValueError:
+            continue
+        else:
+            return pyramid_height
+            break
+#for pyramid_level in range(pyramid_height):
+
